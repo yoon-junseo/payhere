a4ae7f01f8bbf61b54e78dab79a51133872899e3
jun5e00 <ysj6844@naver.com>
2022-09-08 21:43:15 +0900

    feat: repos, search API
    1. repository 검색 API
    2. issue 목록 조회 API
    #1

---
+++ src/lib/api/repos.ts
@@ -0,0 +1,43 @@
+import request from '@/lib/api/request';
+
+import { GetRepositoryIssuesProps, GetRepositoryIssuesParams, IssueState } from '@/lib/api/types';
+
+const reposBaseUrl = '/repos';
+
+const reposUrl = {
+  listRepositoryIssues: ({ owner, repo }: GetRepositoryIssuesProps) => `${reposBaseUrl}/${owner}/${repo}/issues`,
+};
+
+export const getRepositoryIssues = ({
+  owner,
+  repo,
+  milestone,
+  state = 'open',
+  assignee,
+  creator,
+  mentioned,
+  labels,
+  sort = 'created',
+  direction = 'desc',
+  since,
+  per_page = 10,
+  page = 1,
+}: GetRepositoryIssuesProps & Partial<GetRepositoryIssuesParams>) =>
+  request<IssueState[], null, Partial<GetRepositoryIssuesParams>>(
+    'GET',
+    reposUrl.listRepositoryIssues({ owner, repo }),
+    null,
+    {
+      milestone,
+      state,
+      assignee,
+      creator,
+      mentioned,
+      labels,
+      sort,
+      direction,
+      since,
+      per_page,
+      page,
+    },
+  ).then((res) => res.data);
---
+++ src/lib/api/search.ts
@@ -0,0 +1,29 @@
+import request from '@/lib/api/request';
+
+import { GetSearchRepositoriesRequestProps, SearchRepositoriesResponse } from '@/lib/api/types';
+
+const searchBaseUrl = '/search';
+
+const searchUrl = {
+  searchRepositories: `${searchBaseUrl}/repositories`,
+};
+
+export const getSearchRepositories = ({
+  q,
+  sort = 'best-match',
+  order = 'desc',
+  per_page = 10,
+  page = 1,
+}: GetSearchRepositoriesRequestProps) =>
+  request<SearchRepositoriesResponse, null, GetSearchRepositoriesRequestProps>(
+    'GET',
+    searchUrl.searchRepositories,
+    null,
+    {
+      q,
+      sort,
+      order,
+      per_page,
+      page,
+    },
+  ).then((res) => res.data);
