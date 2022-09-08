872f5ac6cd3c6f6a30669f73b7cfdd55575d5642
jun5e00 <ysj6844@naver.com>
2022-09-08 21:38:23 +0900

    feat: react-query 설정
    #1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <div>Junseo's project setting</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
