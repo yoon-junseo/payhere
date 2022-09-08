02149ac626f4ae6ab4549abea002348b471b3e5f
jun5e00 <ysj6844@naver.com>
2022-09-08 21:42:17 +0900

    feat: API 요청에 필요한 type 설계
    1. GET /search/repositories에 필요한 type
    2. GET /repos/{owner}/{repo}/issues에 필요한 type
    + 추후에 타입 리팩토링 필요
    #1

---
+++ src/lib/api/types/url.ts
@@ -0,0 +1,69 @@
+export interface UrlState {
+  url: string | null;
+  html_url: string | null;
+}
+
+export interface UserUrlState extends UrlState {
+  avatar_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+}
+
+export interface RepositoryUrlState {
+  forks_url: string;
+  keys_url: string;
+  collaborators_url: string;
+  teams_url: string;
+  hooks_url: string;
+  issue_events_url: string;
+  events_url: string;
+  assignees_url: string;
+  branches_url: string;
+  tags_url: string;
+  blobs_url: string;
+  git_tags_url: string;
+  git_refs_url: string;
+  trees_url: string;
+  statuses_url: string;
+  languages_url: string;
+  stargazers_url: string;
+  contributors_url: string;
+  subscribers_url: string;
+  subscription_url: string;
+  commits_url: string;
+  git_commits_url: string;
+  comments_url: string;
+  issue_comment_url: string;
+  contents_url: string;
+  compare_url: string;
+  merges_url: string;
+  archive_url: string;
+  downloads_url: string;
+  issues_url: string;
+  pulls_url: string;
+  milestones_url: string;
+  notifications_url: string;
+  labels_url: string;
+  releases_url: string;
+  deployments_url: string;
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  svn_url: string;
+  homepage: string | null;
+  mirror_url: string | null;
+}
+
+export interface IssuesUrlState extends UrlState {
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+}
