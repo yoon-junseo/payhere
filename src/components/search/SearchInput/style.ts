8812b0dd5cd8c167111d6e63a379992b14bd90b3
jun5e00 <ysj6844@naver.com>
2022-09-09 21:42:10 +0900

    feat: SearchInput
    1. 검색창
    #5

---
+++ src/components/search/SearchInput/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import { boxDesign } from '@/styles/common';
+
+export const Container = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+
+  width: 290px;
+  height: 40px;
+
+  ${boxDesign()};
+`;
