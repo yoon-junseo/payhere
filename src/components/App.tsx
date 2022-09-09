c208c2bc4e0bf2ee7849f3125841ba71614f9f18
jun5e00 <ysj6844@naver.com>
2022-09-09 15:26:36 +0900

    feat: Router
    1. 라우팅 처리
    #3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import Router from '@/Router';
+
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen />
+      <Router />
     </QueryClientProvider>
   );
 };
