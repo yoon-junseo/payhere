57256dc006ec3fc8cfc6b04e7c0c7c6def70be04
jun5e00 <ysj6844@naver.com>
2022-09-09 21:40:26 +0900

    design: boxDesign
    1. 배경색, 보더 값, 그림자 설정하는 공용 css
    #5

---
+++ src/styles/common/index.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components';
+
+import Theme from '@/styles/Theme';
+
+export const boxDesign = (borderRadius?: number) => css`
+  background: ${Theme.M_1};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: ${borderRadius ?? 6}px;
+`;
