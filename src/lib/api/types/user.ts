7062250338575aac131363d551ea0442fa0a8757
jun5e00 <ysj6844@naver.com>
2022-09-10 17:14:24 +0900

    fix: types/user
    1. 임시로 불필요한 타입 주석처리
    2. 오타 수정
    #7

--- src/lib/api/types/user.ts
+++ src/lib/api/types/user.ts
@@ -1,16 +1,16 @@
 import { Id, NodeId, UrlState, UserUrlState } from '@/lib/api/types';
 
-export interface OwnerState extends Id, NodeId, UrlState, UserUrlState {
+export interface OwnerState extends Id, NodeId, UrlState, UserState {
   title: string;
   description: string | null;
 }
 
 export interface UserState extends Id, NodeId, UrlState, UserUrlState {
   login: string;
-  name: string | null;
-  email: string | null;
+  // name: string | null;
+  // email: string | null;
   gravatar_id: string;
   type: string;
   site_admin: boolean;
-  starred_at: string;
+  // starred_at: string;
 }
