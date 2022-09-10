f2960023dd0917fd48e42589dc2cc1c908765ca7
jun5e00 <ysj6844@naver.com>
2022-09-10 17:06:29 +0900

    feat: Icon 추가
    1. Bookmark
    2. Closed
    3. FilledBookmark
    4. Fork
    5. Opened
    6. Watcher
    #7

---
+++ src/components/common/Icon/Icons/Bookmark.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Bookmark = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      width="96px"
+      height="96px"
+      viewBox="0 0 96 96"
+      enable-background="new 0 0 96 96"
+      xmlSpace="preserve"
+    >
+      <image
+        id="image0"
+        width="96"
+        height="96"
+        x="0"
+        y="0"
+        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAQAAABIkb+zAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN RQfmCQkSNyTPd0w/AAACsElEQVR42u2bTWsTQRiAn9hYxC5+QCmiFkQ0IAErLSgePChU9CD4C/IX cgmKnnoUrAnRfyB+XDxYRKmtHjyIBw9CQbAoglhoSyo9NFVqbNeDKLuZ3WSSzOys8D57CZud932f ZGZ3sjsBQRAEQRDckem6ZY7LjHOQYQZ6yL/OVxaY4TEfk9TO8wTf8DbLaDLFZ6myabx8H59NKmRt l7+HWSvF/92es9tm+X08s1q+j8+MzW+har18H5+yrfJHm/r+ItcY6ekcBAOMcJ2lprFwwo5AuPc/ 6LH0IB4PQ7GnbZSfC6W438MVJIpMk8IR8wJXQp3HMx7fC3Wkkm6zbdoJxgOvb1M3LlDnTkw2Q8wH Pp/j5sMDI4EMH8yHXwuEN9+BALxAhjXdRvpD0e+qVWd0kUN/DKQUEXCNCLhGBFwjAq4RAdeIgGtE wDUi4BoRcI0IuEYEXCMCrhEB14iAa0TANSLgGhFwjQi4RgRa4FGmRo2ypefKHRJciqHDWODZ/mdO W8lhTSBDkY1QiwYTGt92SgSGYhamveTA/yBwnsXYxWQ1LqVbYAdVttqsiLvLzrQK5JlTyl1lVdk3 Rz6NAgXWlVLfcJhhXin7f1CMXI3iTGCQKaXIX9xgOwB9XOWn8v40+9IicI4FpbwvnAkdc5JPyjFL XHQvkGUiYiX1I/Yq7XdxTzluiyr9LgUO8Vop6jvF2BiF0MK1P9tbcq4Eoss52jJKa+UEBXQ6RDSt Ol1iAqcih+QF7VhnY4Z9YgINJf0Ugx1FizrxNpIT0LsstaNAvUXUxATe97CO9xjvXAu0mprpED/9 S0Cg3eRYl+gJuHWBF+w3FnuIp/YFVgLBNygZ/wNEKfQjtGZe4Na/4POMmQ9P+DbATfPh+5lkmRUq Fm+SeFT4xjKTGld0QRAEQRBSwG/ZkZfGiYgpTwAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMi0wOS0w OVQxNjo1NTozNiswMjowMMw2n44AAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjItMDktMDlUMTY6NTU6 MzYrMDI6MDC9aycyAAAAAElFTkSuQmCC"
+      />
+    </svg>
+  );
+};
+
+export default Bookmark;
