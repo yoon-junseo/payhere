5b683785edb4fb65ffe3485faeaa1378961a7999
jun5e00 <ysj6844@naver.com>
2022-09-11 09:35:12 +0900

    feat: RepositoryPage
    1. 페이지 구성
    2. 파일 위치 변경
    #11

---
+++ src/pages/RepositoryPage/index.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+import { repositoryList, openedIssue } from '@/mock';
+
+import AppLayout from '@/components/layout/AppLayout';
+
+import StoredRepositoryList from '@/components/repository/StoredRepositoryList';
+
+import Issue from '@/components/issue/Issue';
+
+import Text from '@/components/common/Text';
+import Spacer from '@/components/common/Spacer';
+
+import * as S from './style';
+
+const RepositoryPage = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const [height, setHeight] = useState<number>(0);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    setHeight(containerRef.current.clientHeight);
+  }, [containerRef]);
+
+  return (
+    <AppLayout>
+      <S.Container ref={containerRef}>
+        <StoredRepositoryList repositoryList={repositoryList} />
+        <S.Section>
+          <Text fontSize={18} fontWeight={700}>
+            Issue 목록
+          </Text>
+          <Spacer y={45} />
+          <S.IssueListContainer height={height - 100}>
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+            <Issue {...openedIssue} />
+          </S.IssueListContainer>
+        </S.Section>
+      </S.Container>
+    </AppLayout>
+  );
+};
+
+export default RepositoryPage;
