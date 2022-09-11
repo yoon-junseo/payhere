8a6866167e642b3e4c75967fa915394daa4a34a1
jun5e00 <ysj6844@naver.com>
2022-09-11 18:14:12 +0900

    fix: SearchInput
    1. props로 값을 전달 받게 수정
    2. 값이 debouncing 중이라면 스피너가 보이게 변경
    #13

--- src/components/search/SearchInput/index.stories.tsx
+++ src/components/search/SearchInput/index.stories.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 
-import SearchInput from '@/components/search/SearchInput';
+import SearchInput, { SearchInputProps } from '@/components/search/SearchInput';
 
 export default {
   component: SearchInput,
   title: 'search/SearchInput',
 };
 
-const Template: Story = () => <SearchInput />;
+const Template: Story<SearchInputProps> = (args: SearchInputProps) => <SearchInput {...args} />;
 
 export const Default = Template.bind({});
