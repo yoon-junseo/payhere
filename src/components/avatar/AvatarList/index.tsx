2e8293565918469efa19440a7be7b632c9306889
jun5e00 <ysj6844@naver.com>
2022-09-12 10:09:11 +0900

    feat: AvatarList에 링크 추가
    #17

--- src/components/avatar/AvatarList/index.tsx
+++ src/components/avatar/AvatarList/index.tsx
@@ -14,7 +14,9 @@ const AvatarList = ({ assignees }: AvatarListProps) => {
   return (
     <S.Container>
       {assignees.map((assignee) => (
-        <Avatar key={assignee.id} src={assignee.avatar_url} />
+        <a href={assignee.html_url} target="_blank">
+          <Avatar key={assignee.id} src={assignee.avatar_url} />
+        </a>
       ))}
     </S.Container>
   );
