2326fcccbd4c47cdf2a56a1f2d3c6fd8f42c8231
jun5e00 <ysj6844@naver.com>
2022-09-13 22:36:49 +0900

    feat: 로그인 만료 에러 처리
    1. firebase로 깃허브 자체 cors 에러를 해결했다. firebase에서는 refreshToken으로 accessToken을 발급 받는 함수가 없어서 다시 로그인시켰다.
    #19

--- src/lib/constants/errorMessage.ts
+++ src/lib/constants/errorMessage.ts
@@ -1,5 +1,6 @@
 const errorMessage = {
   API_RATE_LIMIT: 'API rate limit exceeded',
+  BAD_CREDENTIALS: 'Bad credentials',
 };
 
 export default errorMessage;
--- src/lib/utils/errorHandler.ts
+++ src/lib/utils/errorHandler.ts
@@ -6,14 +6,23 @@ import githubLoginHandler from '@/lib/utils/githubLoginHandler';
 
 const errorHandler = (error: AxiosError) => {
   if (!error.response) return;
-  const { data } = error.response as unknown as DataState;
 
-  if (data.message.includes(errorMessage.API_RATE_LIMIT)) {
+  const { data: errorData } = error.response as unknown as DataState;
+
+  if (errorData.message.includes(errorMessage.API_RATE_LIMIT)) {
     const confirm = window.confirm(
-      `깃허브 API 호출 횟수를 초과했습니다!\n 로그인을 하시면 호출 가능한 횟수가 증가합니다`,
+      '깃허브 API 호출 횟수를 초과했습니다!\n 로그인을 하시면 호출 가능한 횟수가 증가합니다\n 로그인 하시겠습니까?',
     );
 
     if (!confirm) return;
+
+    githubLoginHandler();
+
+    return;
+  }
+
+  if (errorData.message === errorMessage.BAD_CREDENTIALS) {
+    alert('로그인 시간이 만료 되었습니다!\n ');
 
     githubLoginHandler();
 
